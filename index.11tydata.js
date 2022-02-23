d898215def722e86f57112156b2a284c7ef92517
Ayush Tewari <atewari@dhcp-10-29-110-194.dyn.MIT.EDU>
2022-02-22 22:21:02 -0500

    pubs added

--- index.11tydata.js
+++ index.11tydata.js
@@ -6,29 +6,46 @@
 // lets us flexibly prepare the data and simplifies the template.
 const AyushTewari = "Ayush Tewari";
 const AbdallahDib = "Abdallah Dib".link("https://www.interdigital.com/talent/?id=38");
+const AdamKortylewski = "Adam Kortylewski".link("https://adamkortylewski.com/");
 const BerndBickel = "Bernd Bickel".link("http://berndbickel.com/");
+const BernhardEgger = "Bernhard Egger".link("https://eggerbernhard.ch/");
 const ChristianTheobalt = "Christian Theobalt".link("https://people.mpi-inf.mpg.de/~theobalt/");
 const ChristophLassner = "Christoph Lassner".link("https://christophlassner.de/");
+const DanielCremers = "Daniel Cremers".link("https://vision.in.tum.de/members/cremers");
 const EdgarTretschk = "Edgar Tretschk".link("http://people.mpi-inf.mpg.de/~tretschk/");
+const FengXu = "Feng Xu".link("http://cgcad.thss.tsinghua.edu.cn/xufeng/");
+const FlorianBernard = "Florian Bernard".link("https://sites.google.com/site/fbernardpi/");
 const GereonFox = "Gereon Fox".link("https://people.mpi-inf.mpg.de/~gfox/");
 const HansPeterPfister = "Hanspeter Pfister".link("https://vcg.seas.harvard.edu/people/hanspeter-pfister");
 const HansPeterSeidel = "Hans-Peter Seidel".link("https://people.mpi-inf.mpg.de/~hpseidel/english.html");
+const IkhsanulHabibie = "Ikhsanul Habibie".link("https://www.mpi-inf.mpg.de/~ihabibie/");
 const MarcHabermann = "Marc Habermann".link("http://people.mpi-inf.mpg.de/~mhaberma/");
 const MallikarjunBR = "Mallikarjun B R".link("http://people.mpi-inf.mpg.de/~mbr");
 const MichaelZollhoefer = "Michael Zollhoefer".link("https://zollhoefer.com/");
 const MohamedElgharib = "Mohamed Elgharib".link("https://people.mpi-inf.mpg.de/~elgharib/");
-const LinjieLiu = "Linjie Liu".link("http://people.mpi-inf.mpg.de/~atewari/");
-const LingjieLiu = "Linjie Liu".link("https://lingjie0206.github.io/");
+const LinjieLyu = "Linjie Lyu".link("http://people.mpi-inf.mpg.de/~atewari/");
+const LingjieLiu = "Lingjie Liu".link("https://lingjie0206.github.io/");
 const LouisChevallier = "Louis Chevallier".link("https://scholar.google.fr/citations?user=hC_BTU8AAAAJ&hl=en");
+const VolkerBlanz = "Volker Blanz".link("http://www.grk1564.uni-siegen.de/de/blanz-volker");
+const StefanieWuhrer = "Stefanie Wuhrer".link("http://morpheo.inrialpes.fr/~wuhrer/");
+const SamiRomdhani = "Sami Romdhani".link("https://www.linkedin.com/in/sami-romdhani-09775827/");
+const TaeHyunOh = "Tae-Hyun Oh".link("https://scholar.google.com/citations?user=dMCBjeIAAAAJ&hl=en");
+const TarunYenamandra = "Tarun Yenamandra".link("https://vision.in.tum.de/members/yenamand");
+const ThaboBeeler = "Thabo Beeler".link("https://thabobeeler.com/");
+const ThomasVetter = "Thomas Vetter".link("https://gravis.dmi.unibas.ch/");
+const TimoBolkart = "Timo Bolkart".link("https://ps.is.mpg.de/person/tbolkart");
 const TimWeyrich = "Tim Weyrich".link("http://reality.cs.ucl.ac.uk/weyrich.html");
 const VladislavGolyanik = "Vladislav Golyanik".link("http://people.mpi-inf.mpg.de/~golyanik/");
+const WillSmith = "Will Smith".link("https://www-users.cs.york.ac.uk/wsmith/");
 const WojciechMatusik = "Wojciech Matusik".link("https://cdfg.csail.mit.edu/wojciech");
+const YuxiaoZhou = "Yuxiao Zhou".link("https://calciferzh.github.io/");
 
 
 /////Conferences
 const ICCV = "ICCV"
 const CVPR = "CVPR"
 const SIGGRAPH = "SIGGRAPH"
+const TOG = "ACM Transactions on Graphics"
 const BMVC = "BMVC"
 
 function authorList(authors) {
@@ -59,7 +76,7 @@ module.exports = {
         {
             title: "Efficient and Differentiable Shadow Computation for Inverse Problems",
             teaser: "assets/EfficientShadows.png",
-            authors: authorList([LinjieLiu, MarcHabermann, LingjieLiu, MallikarjunBR, AyushTewari, ChristianTheobalt]),
+            authors: authorList([LinjieLyu, MarcHabermann, LingjieLiu, MallikarjunBR, AyushTewari, ChristianTheobalt]),
             conference: ICCV + " 2021",
             data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/DifferentiableShadows/"), "[paper]".link("https://arxiv.org/pdf/2104.00359.pdf")].join(" ")
         },
@@ -77,6 +94,41 @@ module.exports = {
             conference: SIGGRAPH + " 2021",
             data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/PhotoApp/"), "[paper]".link("https://arxiv.org/pdf/2103.07658.pdf")].join(" ")
         },
-
+        {
+            title: "i3DMM: Deep Implicit 3D Morphable Model of Human Heads",
+            teaser: "assets/i3dmm.png",
+            authors: authorList([TarunYenamandra, AyushTewari, FlorianBernard, HansPeterSeidel, MohamedElgharib, DanielCremers, ChristianTheobalt]),
+            conference: CVPR + " 2021 (Oral)",
+            data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/i3DMM/"), "[paper]".link("https://arxiv.org/pdf/2011.14143.pdf"), "[code]".link("https://github.com/tarun738/i3DMM")].join(" ")
+        },
+        {
+            title: "Monocular Reconstruction of Neural Face Reflectance Fields",
+            teaser: "assets/reflectance.png",
+            authors: authorList([MallikarjunBR, AyushTewari, TaeHyunOh, TimWeyrich, BerndBickel, HansPeterSeidel, HansPeterPfister, WojciechMatusik, MohamedElgharib, ChristianTheobalt]),
+            conference: CVPR + " 2021",
+            data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/FaceReflectanceFields/"), "[paper]".link("https://arxiv.org/pdf/2008.10247.pdf")].join(" ")
+        },
+        {
+            title: "Learning Complete 3D Morphable Face Models from Images and Videos",
+            teaser: "assets/lemomo.png",
+            authors: authorList([MallikarjunBR, AyushTewari, HansPeterSeidel, MohamedElgharib, ChristianTheobalt]),
+            conference: CVPR + " 2021",
+            data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/LeMoMo/"), "[paper]".link("https://arxiv.org/pdf/2010.01679.pdf")].join(" ")
+        },
+        {
+            title: "Monocular Real-time Full Body Capture with Inter-part Correlations",
+            teaser: "assets/yuxiao.jpeg",
+            authors: authorList([YuxiaoZhou, MarcHabermann, IkhsanulHabibie, AyushTewari, ChristianTheobalt, FengXu]),
+            conference: CVPR + " 2021",
+            data: ["[project page]".link("http://gvv.mpi-inf.mpg.de/projects/2021-cvpr-full-body-capture/"), "[paper]".link("https://arxiv.org/pdf/2012.06087.pdf")].join(" ")
+        },
+        {
+            title: "3D Morphable Face Models - Past, Present and Future3dmm",
+            teaser: "assets/3dmm.png",
+            authors: authorList([BernhardEgger, WillSmith, AyushTewari, StefanieWuhrer, MichaelZollhoefer, ThaboBeeler, FlorianBernard, TimoBolkart, AdamKortylewski, SamiRomdhani, ChristianTheobalt, VolkerBlanz, ThomasVetter]),
+            conference: TOG + " 2021",
+            data: ["[arXiv]".link("https://arxiv.org/abs/1909.01815")].join(" ")
+        },
     ],
+    metadata: ["[google scholar]".link("https://scholar.google.com/citations?user=pDnzpeoAAAAJ&hl=en"), "[CV]"].join(" "),
 };
