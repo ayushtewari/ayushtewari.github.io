e763e62ab3d8004982f9f7ea1a33b76d8e1a4bfe
Ayush Tewari <at@at2164mac.eng.cam.ac.uk>
2026-01-14 12:02:55 +0000

    update

--- index.11tydata.js
+++ index.11tydata.js
@@ -40,6 +40,9 @@ const HansPeterSeidel = "Hans-Peter Seidel".link("https://people.mpi-inf.mpg.de/
 const HyeongwooKim = "Hyeongwoo Kim".link("http://www.mpi-inf.mpg.de/~hkim/");
 const IkhsanulHabibie = "Ikhsanul Habibie".link("https://www.mpi-inf.mpg.de/~ihabibie/");
 const JasonSaragih = "Jason Saragih".link("http://jsaragih.org/Home_Page.html");
+const JeffreyHu = "Jeffrey Hu".link("https://jefequien.github.io/");
+const JieyingChen = "Jieying Chen".link("https://www.linkedin.com/in/jieying-chen-5bb4381a1/");
+const JoanLasenby = "Joan Lasenby".link("https://www.eng.cam.ac.uk/profiles/jl221");
 const JoshTenenbaum = "Joshua B. Tenenbaum".link("http://web.mit.edu/cocosci/josh.html");
 const JonBarron = "Jonathan T. Barron".link("https://jonbarron.info/");
 const JulienGaubil = "Julien Gaubil".link("https://www.jgaubil.com/");
@@ -200,6 +203,7 @@ module.exports = {
             name: "Jieying Chen",
             role: "with Joan Lasenby",
             photo: "assets/students/jieying_chen.jpg",
+            url: "https://www.linkedin.com/in/jieying-chen-5bb4381a1/"
         },
         {
             name: "Jeffrey Hu",
@@ -209,6 +213,13 @@ module.exports = {
         },
     ],
     publications: [
+        {
+            title: "Efficient Camera-Controlled Video Generation of Static Scenes via Sparse Diffusion and 3D Rendering",
+            teaser: "projects/srender/videos/teaser.mp4",
+            authors: authorList([JieyingChen, JeffreyHu, JoanLasenby, AyushTewari]),
+            conference: "arXiv, 2025",
+            data: ["[project page]".link("projects/srender/")].join(" ")
+        },
         {
             title: "VDAWorld: World Modelling via VLM-Directed Abstraction and Simulation",
             teaser: "assets/VDAWorld4.mp4",
